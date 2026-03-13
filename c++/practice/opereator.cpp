5c36ff43a7711053d057e5b89352382c0dcddeee
umairrrkhan <umair64066@gmail.com>
2026-03-13 23:33:46 +0530

    learned operator

---
+++ c++/practice/opereator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    x = 12; 
+    int y;
+    y = 12;
+    int sum = x+y;
+    cout<< sum << endl ;
+
+   char a = '1' ;
+   char b = '2'; 
+    cout<< a+ b ;
+
+    int crow ;
+    cout<<"enter the number     ";
+    cin >> crow;
+    cout<< crow<<endl;
+    int y2;
+    cout<<"home ";
+    cin>>y2;
+    cout<<y2<<endl;
+
+    int k = 10 , m = 14 ;
+    cout<< m;
+
+    int remainder = k%m;
+    cout<<remainder;
+    bool isEqual = (a==b);
+    cout<<isEqual;
+    bool bothPositive= (a> 0 && b >0    );
+    cout<< bothPositive;
+    
+
+}
