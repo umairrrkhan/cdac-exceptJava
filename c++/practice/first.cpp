320a96872c114a66129c916275f9fc7dc050d534
umairrrkhan <umair64066@gmail.com>
2026-03-13 16:32:31 +0530

    add first cpp practice file

---
+++ c++/practice/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int Number;
+    cout<< "enter the number : ";
+    cin >> Number;
+    cout <<Number ;
+    cout<< "hello world"<<endl;
+    
+}
