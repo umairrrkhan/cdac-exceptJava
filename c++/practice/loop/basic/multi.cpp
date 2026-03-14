11b911114eeaef0a3695f8c85ef8b9c4f483c4b9
umairrrkhan <umair64066@gmail.com>
2026-03-14 13:43:34 +0530

    multi problem these are all too basic

---
+++ c++/practice/loop/basic/multi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int multi;
+    cout<<"enter the number";
+    cin>>multi;
+
+    for (int i =1 ; i<=10 ; i++){
+        cout<<multi*i<<endl;
+    }
+}
