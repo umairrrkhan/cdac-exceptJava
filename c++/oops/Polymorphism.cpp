ad6c564731336e0350ec206815bf496cb4840af9
umairrrkhan <umair64066@gmail.com>
2026-03-16 22:38:29 +0530

    polymorphism is intresting intersting to learn

---
+++ c++/oops/Polymorphism.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Shape{
+
+    public :
+    virtual void draw(){
+        cout<<"draw    k   "<<endl;
+    }
+
+    virtual ~Shape() {}
+
+};
+
+
+class Cirlce : public Shape{
+
+    void draw() override{
+        cout<<" this is black cirlce "<<endl;
+    }
+
+};
+
+class rectangel : public Shape{
+    void draw() override{
+        cout<<" this is the rectangle "<<endl;
+    }
+};
+
+int main(){
+
+    Shape* s1 = new Cirlce();
+    Shape* s2 = new rectangel();
+
+    s1->draw();
+    s2->draw();
+
+    delete s1;
+    delete s2;
+
+    return 0 ;
+}
