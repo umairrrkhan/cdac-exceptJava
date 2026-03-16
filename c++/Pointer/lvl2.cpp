d4c876cdc855e919fbe68b5c620d490ff4a144f5
umairrrkhan <umair64066@gmail.com>
2026-03-16 23:20:10 +0530

    pointer task 2 is done

---
+++ c++/Pointer/lvl2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int size;
+    cout<<" enter number ";
+    cin>>size;
+
+    int* number = new int[size];
+
+    for (int i = 0 ; i< size ; i++){
+        cin>>  *(number + i);
+    }
+
+    cout<<endl;
+
+
+    for (int i = 0 ; i< size ; i++){
+        cout<<*(number + i );
+    }
+
+      delete[] number;  
+    number = nullptr;
+}
