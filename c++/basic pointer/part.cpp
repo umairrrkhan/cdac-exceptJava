55df1134f1ab984777276dbe4c2c361ff76bf60d
umairrrkhan <umair64066@gmail.com>
2026-03-31 23:38:36 +0530

    part 1

---
+++ c++/basic pointer/part.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    long vr = 45;
+
+    long* ptr = &vr;
+
+    cout << ptr <<"what the fuck is this not sure but its not that hard like people told me     " <<endl;
+}
